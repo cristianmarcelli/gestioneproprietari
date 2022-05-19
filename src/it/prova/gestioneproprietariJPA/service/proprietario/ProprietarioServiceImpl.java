8ccadce1b6e99b997487f0b161b4ab9aeaf29a46
Cristian <cristianma98@gmail.com>
2022-05-19 22:04:14 +0200

    completata struttura ProprietarioServiceImpl

--- src/it/prova/gestioneproprietariJPA/service/proprietario/ProprietarioServiceImpl.java
+++ src/it/prova/gestioneproprietariJPA/service/proprietario/ProprietarioServiceImpl.java
@@ -1,5 +1,56 @@
 package it.prova.gestioneproprietariJPA.service.proprietario;
 
+import java.util.List;
+
+import it.prova.gestioneproprietariJPA.dao.proprietario.ProprietarioDAO;
+import it.prova.gestioneproprietariJPA.model.Proprietario;
+
 public class ProprietarioServiceImpl implements ProprietarioService {
+
+	@Override
+	public List<Proprietario> listAllProprietari() throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public Proprietario caricaSingoloProprietario(Long id) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public void aggiorna(Proprietario proprietarioInstance) throws Exception {
+	}
+
+	@Override
+	public void inserisciNuovo(Proprietario proprietarioInstance) throws Exception {
+	}
+
+	@Override
+	public void rimuovi(Long idProprietarioInstance) throws Exception {
+	}
+
+	@Override
+	public List<Proprietario> cercaTuttiIProprietariConNome(String nome) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public List<Proprietario> cercaTuttiIProprietariConCognome(String cognome) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public List<Proprietario> cercaTuttiIProprietariConTargaAutomobileIniziaCon(String targa) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public void setProprietarioDAO(ProprietarioDAO proprietarioDAO) {
+	}
 
 }

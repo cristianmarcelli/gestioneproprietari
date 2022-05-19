21824aff2bb467008f6cb638b273c0a33e00ae66
Cristian <cristianma98@gmail.com>
2022-05-19 22:03:28 +0200

    completato ProprietarioService

--- src/it/prova/gestioneproprietariJPA/service/proprietario/ProprietarioService.java
+++ src/it/prova/gestioneproprietariJPA/service/proprietario/ProprietarioService.java
@@ -1,5 +1,29 @@
 package it.prova.gestioneproprietariJPA.service.proprietario;
 
+import java.util.List;
+
+import it.prova.gestioneproprietariJPA.dao.proprietario.ProprietarioDAO;
+import it.prova.gestioneproprietariJPA.model.Proprietario;
+
 public interface ProprietarioService {
+
+	public List<Proprietario> listAllProprietari() throws Exception;
+
+	public Proprietario caricaSingoloProprietario(Long id) throws Exception;
+
+	public void aggiorna(Proprietario proprietarioInstance) throws Exception;
+
+	public void inserisciNuovo(Proprietario proprietarioInstance) throws Exception;
+
+	public void rimuovi(Long idProprietarioInstance) throws Exception;
+
+	public List<Proprietario> cercaTuttiIProprietariConNome(String nome) throws Exception;
+
+	public List<Proprietario> cercaTuttiIProprietariConCognome(String cognome) throws Exception;
+
+	public List<Proprietario> cercaTuttiIProprietariConTargaAutomobileIniziaCon(String targa) throws Exception;
+
+	// per injection
+	public void setProprietarioDAO(ProprietarioDAO proprietarioDAO);
 
 }

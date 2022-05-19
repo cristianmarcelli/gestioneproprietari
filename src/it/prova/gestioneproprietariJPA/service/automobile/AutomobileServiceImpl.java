54bae66256605382d7ea395e51b6c1e1b14466d5
Cristian <cristianma98@gmail.com>
2022-05-19 21:58:06 +0200

    completata struttura AutomobileServiceImpl

--- src/it/prova/gestioneproprietariJPA/service/automobile/AutomobileServiceImpl.java
+++ src/it/prova/gestioneproprietariJPA/service/automobile/AutomobileServiceImpl.java
@@ -1,5 +1,57 @@
 package it.prova.gestioneproprietariJPA.service.automobile;
 
+import java.util.List;
+
+import it.prova.gestioneproprietariJPA.dao.automobile.AutomobileDAO;
+import it.prova.gestioneproprietariJPA.model.Automobile;
+
 public class AutomobileServiceImpl implements AutomobileService {
+
+	@Override
+	public List<Automobile> listAllAutomobili() throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public Automobile caricaSingolaAutomobile(Long id) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public void aggiorna(Automobile automobileInstance) throws Exception {
+	}
+
+	@Override
+	public void inserisciNuovo(Automobile automobileInstance) throws Exception {
+	}
+
+	@Override
+	public void rimuovi(Long idAutomobileInstance) throws Exception {
+	}
+
+	@Override
+	public List<Automobile> cercaTuttiGliAbitantiConMarca(String marca) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public List<Automobile> cercaTuttiGliAbitantiConModello(String modello) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public List<Automobile> cercaTutteLeAutomobiliTramiteCodiceFiscaleProprietario(String codiceFiscale)
+			throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public void setAutomobileDAO(AutomobileDAO automobileDAO) {
+	}
 
 }

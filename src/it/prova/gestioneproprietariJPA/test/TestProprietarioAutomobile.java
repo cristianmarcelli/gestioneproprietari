fc350931638c9e5d8276fc93c99b3c105e1e7888
Cristian <cristianma98@gmail.com>
2022-05-20 12:12:14 +0200

    completato e testato metodo rimuovi() di Automobile

--- src/it/prova/gestioneproprietariJPA/test/TestProprietarioAutomobile.java
+++ src/it/prova/gestioneproprietariJPA/test/TestProprietarioAutomobile.java
@@ -30,9 +30,9 @@ public class TestProprietarioAutomobile {
 //			//STAMPO LA LISTA DELLE AUTOMOBILI PER VEDERE SE è STAT INSERITA CON IL METODO PRECEDENTE
 //			System.out.println(automobileService.listAllAutomobili());
 
-//			testRimozioneAutomobile(proprietarioService, automobileService);
-//			System.out.println(
-//					"In tabella Automobile ci sono " + automobileService.listAllAutomobili().size() + " elementi.");
+			testRimozioneAutomobile(proprietarioService, automobileService);
+			System.out.println(
+					"In tabella Automobile ci sono " + automobileService.listAllAutomobili().size() + " elementi.");
 
 			
 			
@@ -41,8 +41,8 @@ public class TestProprietarioAutomobile {
 //			System.out.println("In tabella Proprietario ci sono " + proprietarioService.listAllProprietari().size()
 //					+ " elementi.");
 			
-			testInserisciProprietario(proprietarioService);
-			System.out.println(proprietarioService.listAllProprietari());
+//			testInserisciProprietario(proprietarioService);
+//			System.out.println(proprietarioService.listAllProprietari());
 
 		} catch (Throwable e) {
 			e.printStackTrace();
@@ -53,48 +53,48 @@ public class TestProprietarioAutomobile {
 	}
 
 	// TEST METODI AUTOMOBILE
-//	private static void testInserisciAutomobile(ProprietarioService proprietarioService, AutomobileService automobileService) throws Exception {
-//		System.out.println(".......testInserisciAutomobile inizio.............");
-//
-//		List<Proprietario> listaProprietariPresenti = proprietarioService.listAllProprietari();
-//		if (listaProprietariPresenti.isEmpty())
-//			throw new RuntimeException("testInserisciAutomobile fallito: non ci sono proprietari a cui collegarci ");
-//		
-//		Automobile nuovaAutomobile = new Automobile("Nissan", "Micra", "JM676AE", 2021);
-//		if (nuovaAutomobile.getId() != null)
-//			throw new RuntimeException("testInserisciAutomobile fallito: record già presente ");
-//
-//		automobileService.inserisciNuovo(nuovaAutomobile);
-//		nuovaAutomobile.setProprietario(listaProprietariPresenti);
-//
-//		if (nuovaAutomobile.getId() == null)
-//			throw new RuntimeException("testInserisciAutomobile fallito ");
-//
-//		System.out.println(".......testInserisciAutomobile fine: PASSED.............");
-//	}
-	
-//	private static void testRimozioneAutomobile(ProprietarioService proprietarioService,
-//			AutomobileService automobileService)
-//			throws Exception {
-//		System.out.println(".......testRimozioneAutomobile inizio.............");
-//
-//		List<Proprietario> listaProprietarioiPresenti = proprietarioService.listAllProprietari();
-//		if (listaProprietarioiPresenti.isEmpty())
-//			throw new RuntimeException("testRimozioneAutomobile fallito: non ci sono automobili a cui collegarci ");
-//
-//		Automobile nuovaAutomobile = new Automobile("Ferrari", "G50", "MH729HN", 2008);
-//		
-//		nuovaAutomobile.setProprietario(listaProprietarioiPresenti.get(0));
-//
-//		automobileService.inserisciNuovo(nuovaAutomobile);
-//
-//		Long idAAutomobileInserito = nuovaAutomobile.getId();
-//		automobileService.rimuovi(idAAutomobileInserito);
+	private static void testInserisciAutomobile(ProprietarioService proprietarioService, AutomobileService automobileService) throws Exception {
+		System.out.println(".......testInserisciAutomobile inizio.............");
 
-//		if (automobileService.caricaSingolaAutomobilee(idAAutomobileInserito) != null)
-//			throw new RuntimeException("testRimozioneAutomobile fallito: record non cancellato ");
-//		System.out.println(".......testRimozioneAutomobile fine: PASSED.............");
-//	}
+		List<Proprietario> listaProprietariPresenti = proprietarioService.listAllProprietari();
+		if (listaProprietariPresenti.isEmpty())
+			throw new RuntimeException("testInserisciAutomobile fallito: non ci sono proprietari a cui collegarci ");
+		
+		Automobile nuovaAutomobile = new Automobile("Nissan", "Micra", "JM676AE", 2021);
+		if (nuovaAutomobile.getId() != null)
+			throw new RuntimeException("testInserisciAutomobile fallito: record già presente ");
+
+		automobileService.inserisciNuovo(nuovaAutomobile);
+		nuovaAutomobile.setProprietario(listaProprietariPresenti.get(0));
+
+		if (nuovaAutomobile.getId() == null)
+			throw new RuntimeException("testInserisciAutomobile fallito ");
+
+		System.out.println(".......testInserisciAutomobile fine: PASSED.............");
+	}
+	
+	private static void testRimozioneAutomobile(ProprietarioService proprietarioService,
+			AutomobileService automobileService)
+			throws Exception {
+		System.out.println(".......testRimozioneAutomobile inizio.............");
+
+		List<Proprietario> listaProprietarioiPresenti = proprietarioService.listAllProprietari();
+		if (listaProprietarioiPresenti.isEmpty())
+			throw new RuntimeException("testRimozioneAutomobile fallito: non ci sono automobili a cui collegarci ");
+
+		Automobile nuovaAutomobile = new Automobile("Ferrari", "G50", "MH729HN", 2008);
+		
+		nuovaAutomobile.setProprietario(listaProprietarioiPresenti.get(0));
+
+		automobileService.inserisciNuovo(nuovaAutomobile);
+
+		Long idAAutomobileInserito = nuovaAutomobile.getId();
+		automobileService.rimuovi(idAAutomobileInserito);
+
+		if (automobileService.caricaSingolaAutomobile(idAAutomobileInserito) != null)
+			throw new RuntimeException("testRimozioneAutomobile fallito: record non cancellato ");
+		System.out.println(".......testRimozioneAutomobile fine: PASSED.............");
+	}
 
 	
 	
